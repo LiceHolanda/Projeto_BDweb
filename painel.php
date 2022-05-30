b5e6e4eaab3c8864795471b54e41bdafbf5e222e
Alice <alice.holanda.136@ufrn.edu.br>
2022-05-30 14:38:57 -0300

    recomeçando

---
+++ painel.php
@@ -0,0 +1,69 @@
+<?php
+
+include('protect.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+body {
+background-color: lightcyan;
+}
+
+h1 {font-family: verdana;text-align: center; font-size: 130%;}
+p {color: black; text-align: right; font-size:18px;}
+p.main { text-indent: 50px; text-align: justify; white-space: pre;
+}
+
+
+ 
+</style>
+</head>
+
+<h1>ENCONTRE AQUI A LISTA DE EXERCÍCIOS QUE VOCÊ PRECISA !</h1>
+
+</body>
+
+</html>
+
+</head>
+<body>
+  
+<p style="text-align: center; font-family: verdana; "> Alunos da ECT </p>  
+<p>
+        <a href="logout.php">Sair</a>
+    </p>
+<p> BANCOS DE DADOS PARA WEB - T01 (2022.1) </p>
+
+
+
+<h1 style="color:blue; background-color:white; text-align: center; font-family: verdana; font-size: 25px;"> დ</h1>
+
+
+<p style="color:blue;text-align: center;font-size:25px;"> Faça a sua busca </p>
+<p style="text-align: left;margin-left:20px; "> <b>Disciplina</b> campo para digitar o nome</p>
+<p style="text-align: left;margin-left:20px;"> <b>Unidade</b> 1, 2 ou 3 </p>
+<p style="text-align: left;margin-left:20px;"> <b>Ano</b> ajuda para ter uma referência se por exemplo, foi aplicada em tempos remotos </p>
+
+
+<p style="letter-spacing: 40px;text-align: center;font-size:22px;"><b>C&T</b>
+<p style="text-align: center;font-family: verdana;font-size:15px;word-spacing: 20px;"><i>Ciências   &   Tecnologia </i></p>
+
+
+
+<p style="text-align: center;"><b>Link para:</b> <a href="https://www.w3schools.com"> W3Schools.com</a></p>
+
+
+
+<p class = "main" > 
+    Site criado com o objetivo de ajudar alunos na sua dura trajetória 
+acadêmica com imensas quantidades de exercícios a cada semestre. 
+Aqui você pode encontrar modelos antigos de listas aplicadas fazendo
+uma busca por disciplina, professor, unidade e ano em que ela foi aplicada.  </p>
+   
+    
+</body>
+</html>

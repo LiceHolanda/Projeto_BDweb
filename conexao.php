bb1e51c36b49b33ca4033b514c9978800cb78b57
Yuri <yuri_ravel20@hotmail.com>
2022-05-31 08:06:31 -0300

    conexao arrumada e registro

--- conexao.php
+++ conexao.php
@@ -7,11 +7,11 @@ $host = 'localhost';
 
 $mysqli = new mysqli($host, $usuario, $senha, $database);
 
-if($mysqli->error) {
-    die("Falha ao conectar ao banco de dados: " . $mysqli->error);
-}
-else{
-    echo "Conexao ok";
-
-}
+//if($mysqli->error) {
+ //   die("Falha ao conectar ao banco de dados: " . $mysqli->error);
+//}
+//else{
+//    echo "Conexao ok";
+//
+//}
 ?>

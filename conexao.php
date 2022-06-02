47537b9152889dfc0010ea37ad823329c2ff9bb6
Alice <alice.holanda.136@ufrn.edu.br>
2022-06-02 10:07:25 -0300

    mudei a conexao

--- conexao.php
+++ conexao.php
@@ -10,8 +10,8 @@ $mysqli = new mysqli($host, $usuario, $senha, $database);
 if($mysqli->error) {
     die("Falha ao conectar ao banco de dados: " . $mysqli->error);
 }
-else{
+/*else{
     echo "Conexao ok";
 
 }
-?>
+?> */
